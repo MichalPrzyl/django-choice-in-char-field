8b4ff4a39160b6f3f6d04e1c5417a9fc445aa088
Michał Przyłucki <michal.przyl@gmail.com>
2024-08-25 16:07:26 +0200

    Add app + model + migration

---
+++ project/app/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+my_model_choices = [
+    ('normal', 'Normal'),
+    ('extra', 'Extra'),
+    ('additional', 'Additional'),
+    ('cosmic', 'Cosmic'),
+]
+class MyModel(models.Model):
+    name = models.CharField(max_length=255)
+    type = models.CharField(null=True, choices=my_model_choices, max_length=255)
